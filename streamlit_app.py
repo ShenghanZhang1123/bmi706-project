593ad2f440bf4c6a8f6424c0b584095bdf8ddd93
Shenghan.Zhang <shenghan.zhang1123@gmail.com>
2024-10-01 14:04:04 -0400

    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,8 +123,8 @@ elif section == 'Interaction Effects':
         size='Income Ratio:Q',
         tooltip=['Age', 'BMI', 'Gender', 'Income Ratio']
     ).properties(
-        width=800,
-        height=400,
+        width=1600,
+        height=800,
         title='Interaction of Age, Gender, and BMI'
     ).interactive()
 
