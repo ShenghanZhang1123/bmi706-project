43f4258861d3d3deb9f18e82f2d2eea0ffe951c7
Shenghan.Zhang <shenghan.zhang1123@gmail.com>
2024-10-01 14:14:54 -0400

    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,11 +104,11 @@ elif section == 'Regression Analysis':
     regression_chart = alt.Chart(df).mark_point().encode(
         x=alt.X(variable, type='quantitative'),
         y=alt.Y('BMI', type='quantitative'),
-    ).interactive() + alt.Chart(df).transform_regression(variable, 'BMI').mark_line().encode(
+    ).properties(height=1000).interactive() + alt.Chart(df).transform_regression(variable, 'BMI').mark_line().encode(
         x=variable,
         y='BMI',
         color=alt.value('red')
-    )
+    ).properties(height=1000).interactive()
 
     st.altair_chart(regression_chart, use_container_width=True)
 
@@ -124,8 +124,9 @@ elif section == 'Interaction Effects':
         size='Income Ratio:Q',
         tooltip=['Age', 'BMI', 'Gender', 'Income Ratio']
     ).properties(
+        height=1000,
         title='Interaction of Age, Gender, and BMI'
     ).interactive()
 
-    st.altair_chart(interaction_plot, use_container_width=True, use_container_height=True)
+    st.altair_chart(interaction_plot, use_container_width=False)
 
