a7a64734d5cad5b079787f4a8a5a1571e20393e1
Shenghan.Zhang <shenghan.zhang1123@gmail.com>
2024-10-14 21:00:07 -0400

    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ elif section == 'Group-wise BMI Trend over Age':
     # Group by the selected category and plot the trend
     line_chart = alt.Chart(filtered_df).mark_line().encode(
         x=alt.X('Age'),  # Age on x-axis
-        y=alt.Y('mean(BMI)', scale=alt.Scale(domain=[15, 50])),  # BMI on y-axis
+        y=alt.Y('mean(BMI)', scale=alt.Scale(domain=[15, 60])),  # BMI on y-axis
         color=alt.Color(category + ':O', scale=alt.Scale(scheme='category10'))  # Apply distinct colors
     ).properties(
         height=300,
