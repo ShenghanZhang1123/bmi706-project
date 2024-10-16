d32d5a2c2451fc0e7f204d2366962e22e93ec957
Shenghan.Zhang <shenghan.zhang1123@gmail.com>
2024-10-16 12:50:12 -0400

    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,7 +112,7 @@ elif section == 'Group-wise BMI Comparison':
         title=f'Strip Plot of BMI by {category}'
     ).transform_filter(
         selection
-    ).interactive()
+    )
 
     with col1:
         st.altair_chart(bar_with_error, use_container_width=True)
