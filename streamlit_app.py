e530bb123562286fc611bdcd1517ad5ccccadcf6
Shenghan.Zhang <shenghan.zhang1123@gmail.com>
2024-10-16 13:05:10 -0400

    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -111,7 +111,7 @@ elif section == 'Group-wise BMI Comparison':
     )
 
     # Strip plot with jitter and filtering based on selection
-    strip_plot = chart_base.mark_circle().encode(
+    strip_plot = chart_base.mark_circle(size=100).encode(
         x=alt.X(f'{category}:N', title=category),
         y=alt.Y('BMI:Q', title='BMI'),
         color=alt.Color(f'{category}:N', scale=alt.Scale(scheme='category10'))
